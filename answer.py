0181d98115c7bfec697d3be9ef239adff6bfaea3
Paul Kearney <paulke@udel.edu>
2023-02-10 18:45:34 -0500

    added test/fixed answer

--- answer.py
+++ answer.py
@@ -1,12 +1,31 @@
 
-def answer():
-    def determineEnd(num):
-        if(num==None||num==-999):
-            return True
-        return False
-
-    def getNextInt():
 
 
-    def addNums()
+def sumNums(numbers:[int]):
+    total:int=0
+    for num in numbers:
+        if(int(num)==-999):
+            break;
+        if(int(num)>=0):
+            total=total+int(num)
+    return total
+
+def addNums(lines:list[int]):
+
+
+    # if the first line that indicates the total nums is 0, the list is empty
+    if lines[0]==0:
+        return "EMPTY"
+
+    add= sumNums(lines)
+    return add
+
+
+if __name__ == "__main__":
+    filename = input()
+    with open(filename) as data_file:
+        lines: list[str]
+        lines = data_file.readlines()
+        addNums(lines)
+        # Actually do the work
 
